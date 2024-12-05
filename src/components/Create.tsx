a33dc8a8fed4be2d7ac4190d5dbfc7050052786a
nepidkhadka <dipenkhadka191@gmail.com>
2024-12-05 17:00:06 +0545

    React Hook Form Added

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -4,6 +4,19 @@ import { Student } from "../utils/types";
 import { useStudent } from "../hooks/queries/studentQueries";
 import { useAddStudent, useUpdateStudent } from "../hooks/mutations/studentMutations";
 import { useForm } from "react-hook-form";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const studentSchema = z.object({
+    studentName: z.string().min(5, "Name Must Be Greater Than 1").nonempty("Name is required"),
+    studentEmail: z.string().email("Email is required"),
+    age: z.number().min(18, "Age must be greater than 18").nonnegative("Age must be greater than 0"),
+    phoneNumber: z.string().nonempty("Phone number is required"),
+    address: z.string().nonempty("Address is required"),
+    hobbies: z.string().nonempty("Hobbies is required"),
+    startDate: z.date(),
+    gender: z.enum(["male", "female", "other"], { message: "Gender is required" })
+})
 
 const Create = () => {
 
@@ -15,17 +28,25 @@ const Create = () => {
     const { id } = useParams()
     const { data } = useStudent(id!)
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm<Student>({
+    const { register, handleSubmit, reset, setError, formState: { errors } } = useForm<Student>({
         defaultValues: {
             studentName: "",
             studentEmail: "",
             phoneNumber: "",
+            address: "",
+            hobbies: "",
+            startDate: "",
+            gender: ""
         },
     })
 
     const handleStudentSubmit = async (formData: Student) => {
         if (isEditMode) {
-            await updateStudent(formData)
+            try {
+                await updateStudent(formData)
+            } catch (error) {
+                setError("root", { message: "Something went wrong" })
+            }
         } else {
             await addStudent(formData)
         }
@@ -51,18 +72,44 @@ const Create = () => {
                     <div className="my-5">
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Student Name</label>
                         <input id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com"   {...register("studentName", { required: "Name Is Required", maxLength: 15 })} />
-                        {errors.studentName && <span className="text-xs text-red-600" >Name is required</span>}
+                        {errors.studentName && <span className="text-xs text-red-600" >{errors.studentName?.message}</span>}
                     </div>
                     <div className="mb-5">
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Student Email</label>
                         <input id="email" type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter Student Email"{...register("studentEmail", { required: "Email Is Required", pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, })} />
-                        {errors.studentEmail && <span className="text-xs text-red-600" >Please Check Email</span>}
+                        {errors.studentEmail && <span className="text-xs text-red-600" >{errors.studentEmail?.message}</span>}
                     </div>
                     <div className="mb-5">
                         <label htmlFor="number" className="block mb-2 text-sm font-medium text-gray-900">Phone Number</label>
                         <input id="number" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Eg. 987-0098-009" {...register("phoneNumber", { required: "Phone Number Is Required" })} />
-                        {errors.phoneNumber && <span className="text-xs text-red-600" >Please Check Phone Number</span>}
+                        {errors.phoneNumber && <span className="text-xs text-red-600" >{errors.phoneNumber?.message}</span>}
                     </div>
+                    <div className="mb-5">
+                        <label htmlFor="age" className="block mb-2 text-sm font-medium text-gray-900">Age</label>
+                        <input id="age" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Eg. 18" {...register("age", { required: "Age Is Required", min: { value: 18, message: "Minimum age is 18" } })} />
+                        {errors.age && <span className="text-xs text-red-600" >{errors.age.message}</span>}
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900">Gender</label>
+                        <select className="w-full"  {...register("gender", { required: "Gender Is Required" })} >
+                            <option value="" selected disabled >Select...</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                        {errors.gender && <span className="text-xs text-red-600" >{errors.gender.message}</span>}
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900">Date</label>
+                        <input id="date" type="datetime-local" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  {...register("startDate", { required: "Date is Required" })} />
+                        {errors.startDate && <span className="text-xs text-red-600" >{errors.startDate.message}</span>}
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="hobbies" className={`block mb-2 text-sm font-medium text-gray-900`}>Hobbies</label>
+                        <input id="hobbies" type="text" className={`${errors.hobbies ? "border-red-600" : ""} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5`} placeholder="Eg. Signing, Drawing" {...register("hobbies", { required: "Hobbies Is Required" })} />
+                        {errors.hobbies && <span className="text-xs text-red-600" >{errors.hobbies?.message}</span>}
+                    </div>
+                    {/* {getValues("hobbies") && <div className="text-xs text-red-600" ><span>{getValues("hobbies")}</span></div>} */}
                     {
                         isEditMode ?
                             <button type="submit" disabled={updatePending} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{updatePending ? "Updating Student..." : "Update"}</button>
@@ -70,7 +117,7 @@ const Create = () => {
                             <button type="submit" disabled={addPending} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{addPending ? "Adding Student..." : "Submit"}</button>
                     }
                 </form>
-
+                {errors.root && <span className="text-xs text-red-600" >{errors.root?.message}</span>}
             </div>
         </>
     )
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,24 @@
+// export interface Student {
+//   id: string; // Unique identifier for the student
+//   createdAt: string; // ISO date string
+//   studentName: string; // Name of the student
+//   avatar: string; // URL to the student's avatar image
+//   studentEmail: string; // Email address of the student
+//   phoneNumber: string; // Phone number of the student
+// }
+
 export interface Student {
-  id: string; // Unique identifier for the student
   createdAt: string; // ISO date string
-  studentName: string; // Name of the student
-  avatar: string; // URL to the student's avatar image
-  studentEmail: string; // Email address of the student
-  phoneNumber: string; // Phone number of the student
+  studentName: string;
+  avatar: string; // URL string
+  studentEmail: string;
+  phoneNumber: string;
+  age: number;
+  gender: string;
+  address: string;
+  hobbies: string;
+  startDate: string; // ISO date string
+  id: string; // Unique identifier
 }
 
 export interface Product {
